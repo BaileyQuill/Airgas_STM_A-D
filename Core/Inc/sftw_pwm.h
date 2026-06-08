dc6ffb323d2aef7741aeed025479c887345a679a
Bailey Quill <bapk626@gmail.com>
2026-06-07 22:27:06 -0400

    created rough headers for software/ service based timers and pwm(for leds).

---
+++ Core/Inc/sftw_pwm.h
@@ -0,0 +1,61 @@
+/**
+  * Created on: Jun 7, 2026
+  * Author: Bailey Quill
+  ******************************************************************************
+  * @file           : sftw_pwm.h
+  * @brief          : software/ service based pwm Note currently only supporting 50% duty cycle
+  ******************************************************************************
+  * @attention
+  *
+  * TODO: license/ copyright
+  *
+  ******************************************************************************
+  */
+
+#ifndef CORE_INC_SFTW_PWM_H_
+#define CORE_INC_SFTW_PWM_H_
+
+#include <main.h> /* for chip specific hal drivers */
+#include "sftw_timer.h"
+
+typedef struct {
+	/* GPIOx where x to select the GPIO peripheral for device family */
+	GPIO_TypeDef *gpiox;
+	/* GPIO_Pin port */
+	uint16_t      gpio_pin;
+	GPIO_PinState currentState;
+	SftwTimer_t   timer;
+} SftwPwm_t;
+
+/** @brief 'initializer' for SftwPwm_t 'object'
+ * @param GPIO_TypeDef *gpiox
+ * @param uint16_t      gpio_pin
+ * @return SftwPwm_t 'object'
+ */
+SftwPwm_t SftwPwm(SftwPwm_t *sftwPwm);
+
+/** @brief starts the SftwTimer to start driving the SftwPwm
+ *    gpio starts high
+ * @param *sftwPwm pointer to the software pwm
+ *
+ * @return void
+ */
+void EnableSftwPwm(SftwPwm_t *sftwPwm);
+
+/** @brief stops the SftwTimer and forces the gpio low
+ * @param *sftwPwm pointer to the software pwm
+ *
+ * @return void
+ */
+void DisableSftwPwm(SftwPwm_t *sftwPwm);
+
+/** @brief updates the SftwTimers period in ms
+ *    resizes current timeout to match
+ * @param *sftwPwm pointer to the software pwm
+ * @param period in ms to update the current period to
+ * @return void
+ */
+void UpdateSftwPwm(SftwPwm_t *sftwPwm, uint32_t period);
+
+
+#endif /* CORE_INC_SFTW_PWM_H_ */
---
+++ Core/Inc/sftw_timer.h
@@ -0,0 +1,54 @@
+/**
+  * Created on: Jun 7, 2026
+  * Author: Bailey Quill
+  ******************************************************************************
+  * @file           : sftw_pwm.h
+  * @brief          : software/ service based pwm
+  ******************************************************************************
+  * @attention
+  *
+  * TODO: license/ copyright
+  *
+  ******************************************************************************
+  */
+
+#ifndef CORE_INC_SFTW_PWM_H_
+#define CORE_INC_SFTW_PWM_H_
+
+#include <main.h> /* for chip specific hal drivers */
+#include <stdbool.h>
+
+typedef enum {
+	st_disabled,   /* timer always returns false */
+	st_singleFire, /* can be used for one off timings, or timers that you want to manualy restart (ie timing based off last completed [whatever timers for]) */
+	st_periodic    /* happens every x (SftwTimer_t.period) ms from initial start. */
+	/* Note in this mode if the service falls behind the timer will trigger every time it's called until the time timeToFire catches up to current system up time */
+}SftwTimer_modes;
+
+typedef struct {
+	uint32_t        timeToFire; /* uptime in ms when the timer will fire again */
+	uint32_t        period;     /* time in ms to increment timeToFire by for repeate timers */
+	SftwTimer_modes current_mode;
+} SftwTimer_t;
+
+/** @brief Checks weather the supplied timer is firing
+ *   timers in st_periodic mode will increment their next time to fire when returning true.
+ * @param *timer pointer to the timer to check
+ * @return true if the timer fired.
+ */
+bool TimerUp (SftwTimer_t *timer);
+
+/** @brief re-enables the timer if disabled and sets the timeToFire to current time + period
+ * @param *timer pointer to the timer to reset
+ * @return void
+ */
+void ResetTimer (SftwTimer_t *timer);
+
+/** @brief updates the period and shifts the current timeToFire to match.
+ * @param *timer pointer to the timer to update
+ * @param uint32_t period in ms to update the timer to
+ * @return void
+ */
+void UpdateTimer (SftwTimer_t *timer, uint32_t newPeriod);
+
+#endif /* CORE_INC_SFTW_PWM_H_ */
