39b02b48992a4013a7dad3d491f8410fe52dbbbd
Bailey Quill <bapk626@gmail.com>
2026-06-10 12:18:00 -0400

    added debug flag back and upreved build version

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -41,7 +41,7 @@ typedef enum {
 /* Versioning ----------------------------------------------------------------*/
 /* TODO: make build script/ cmake pass in version info bassed on tags/ branch name */
 #ifndef BUILD_VER_DEBUG
-#define BUILD_VER_DEBUG   false
+#define BUILD_VER_DEBUG   true
 #endif
 
 #ifndef BUILD_VER_VARIANT
@@ -57,7 +57,7 @@ typedef enum {
 #endif
 
 #ifndef BUILD_VER_BUILD
-#define BUILD_VER_BUILD   2
+#define BUILD_VER_BUILD   3
 #endif
 
 /* usefull for ensuring test builds/ unrealeased versions don't leave engineering */
